f3e02d2ade7b5e311cbc5959d3147c5fcc76ca38
Alex de Abreu <alex.abreu.softwaredeveloper@gmail.com>
2024-02-21 11:56:30 -0300

    modificaçoes adicionais2.0

--- atividade aula04/src/index.ts
+++ atividade aula04/src/index.ts
@@ -3,9 +3,9 @@ class Funcionario {
     cargo_funcionario: string;
     especialidade: string;
     endereço_funcionario: string;
-    idade_funcionario: number;
+    idade_funcionario: Date;
     matricula: number;
-    ano_admissao: number;
+    ano_admissao: Date;
     escolaridade_funcionario: string;
 
     constructor(
@@ -13,9 +13,9 @@ class Funcionario {
         cargo_funcionario: string,
         especialidade: string,
         endereço_funcionario: string,
-        idade_funcionario: number,
+        idade_funcionario: Date,
         matricula: number,
-        ano_admissao: number,
+        ano_admissao: Date,
         escolaridade_funcionario: string) {
         this.nome_funcionario = nome_funcionario
         this.cargo_funcionario = cargo_funcionario
@@ -26,32 +26,56 @@ class Funcionario {
         this.ano_admissao = ano_admissao
         this.escolaridade_funcionario = escolaridade_funcionario
     }
+    
+    mostrarDados(){
+        console.log(`----------Dados do Funcionario--------------------------------`);
+        console.log(`Nome do Funcionario: ${this.nome_funcionario}`);
+        console.log(`Cargo: ${this.cargo_funcionario}`);
+        console.log(`Especialidade: ${this.especialidade}`);
+        console.log(`Endereço ${this.endereço_funcionario}`);
+        console.log(`Idade: ${this.idade_funcionario.toLocaleDateString('pt-BR')}`);
+        console.log(`Matricula: ${this.matricula}`);
+        console.log(`Admissão: ${this.ano_admissao.toLocaleDateString('pt-BR')}`);
+        console.log(`Formação: ${this.escolaridade_funcionario}`);
+        console.log('--------------------------------------------------------------');
+    }
+
 }
-const funcionario1 = new Funcionario("João Silva", "Médico", "Cardiologia", "Rua X, Potengi", 35, 123456, 2015, "Graduação em Medicina");
+const funcionario1 = new Funcionario("João Silva", "Médico", "Cardiologista", "Rua X, Potengi", new Date('1970-2-20'), 123456,new Date('2022-2-2'), "Graduação em Medicina");
+console.log(funcionario1.mostrarDados())
 
 class Paciente {
     nome: string;
-    idade: number;
+    idade: Date;
     cpf: string;
     endereço: string;
-    data_nascimento: Date;
     consultas: Funcionario;
     constructor(
         nome: string,
-        idade: number,
+        idade: Date,
         cpf: string,
         endereço: string,
-        data_nascimento: Date,
         consultas: Funcionario) { 
         this.nome = nome
         this.idade = idade
         this.cpf = cpf
         this.endereço = endereço
-        this.data_nascimento = data_nascimento
         this.consultas = consultas
     }
+
+    mostrarDados2(){
+        console.log(`---------------Dados do Paciente------------------------------`);
+        console.log(`Nome do Paciente: ${this.nome}`);
+        console.log(`Data de Nascimento: ${this.idade.toLocaleDateString('pt-BR')}`);
+        console.log(`CPF: ${this.cpf}`);
+        console.log(`Endereço: ${this.endereço}`);
+        console.log(`Consulta: ${funcionario1.especialidade}`);
+        console.log('-------------------------------------------------------------');
+    }
+
 }
-const paciente1 = new Paciente("Carlos Oliveira", 45, "789.123.456-78", "Rua Y, Igapó", new Date("1979-08-15"),funcionario1);
+const paciente1 = new Paciente("Carlos Oliveira",new Date('1988-2-5'), "789.123.456-78", "Rua Y, Igapó",funcionario1);
+console.log(paciente1.mostrarDados2());
 
 class Consulta {
 
@@ -69,7 +93,18 @@ class Consulta {
         this.medico = medico
         this.data_consulta = data_consulta
     }
-}
-const consulta1 = new Consulta(paciente1, 'cardiologista', 'joão silva', new Date('2-22-2024'))
 
-console.log(funcionario1,paciente1,consulta1)
+    mostrarDados3(){
+        console.log(`---------------Dados da Consulta-----------------------------`);
+        console.log(`Nome do Paciente: ${paciente1.nome}`);
+        console.log(`Especialidade: ${funcionario1.especialidade}`);
+        console.log(`Médico: ${funcionario1.nome_funcionario}`);
+        console.log(`Data da Consulta: ${this.data_consulta.toLocaleDateString('pt-BR')}`);
+        console.log(`Consulta: ${funcionario1.especialidade}`);
+        console.log('-------------------------------------------------------------');
+    }
+}
+const consulta1 = new Consulta(paciente1, 'cardiologista', 'joão silva', new Date('2024-2-2'));
+console.log(consulta1.mostrarDados3());
+
+//console.log(funcionario1,paciente1,consulta1)
--- atividade aula04/src/index02.ts
+++ atividade aula04/src/index02.ts
@@ -20,17 +20,33 @@ class CadastroLivros {
         this.disponivel = disponivel
         this.quantidade = quantidade
     }
+
+    mostrarDados(){
+        console.log(`----------Cadastro de livro--------------------------------`);
+        console.log(`Titulo de Livro: ${this.titulo_livro}`);
+        console.log(`Autor: ${this.autor_livro}`);
+        console.log(`Editora: ${this.editora}`);
+        console.log(`Categoria: ${this.categoria}`);
+        console.log(`Disponivel: ${this.disponivel}`)
+        console.log(`Quantidade: ${this.quantidade}`);
+        console.log('-----------------------------------------------------------');
+    }
 }
 const livro1 = new CadastroLivros('Ferrari', 'Brock Yates', 'BestSeller', 'biografia', true, 5)
 const livro2 = new CadastroLivros('O Senhor dos Anéis', 'J.R.R. Tolkien', 'HarperCollins', 'ficção', true, 10);
 const livro3 = new CadastroLivros('Código Da Vinci', 'Dan Brown', 'Arqueiro', 'mistério', true, 7);
 const livro4 = new CadastroLivros('Percy Jackson e o Ladrão de Raios', 'Rick Riordan', 'Intrínseca', 'fantasia', true, 3);
 const livro5 = new CadastroLivros('O Poder do Hábito', 'Charles Duhigg', 'Objetiva', 'autoajuda', true, 8);
-console.log(livro1, livro2, livro3, livro4, livro5)
+console.log(livro1.mostrarDados());
+console.log(livro2.mostrarDados());
+console.log(livro3.mostrarDados());
+console.log(livro4.mostrarDados());
+console.log(livro5.mostrarDados());
+
 
 class Usuarios {
     nome: string;
-    idade: number;
+    idade: Date;
     email: string;
     endereco: string;
     livros_solicitados: CadastroLivros;
@@ -39,7 +55,7 @@ class Usuarios {
 
     constructor(
         nome: string,
-        idade: number,
+        idade: Date,
         email: string,
         endereco: string,
         livros_solicitados: CadastroLivros,
@@ -53,9 +69,19 @@ class Usuarios {
         this.livros_devolvovidos = livros_devolvovidos
         this.data_solicitacao = data_solicitacao
     }
+
+    mostrarDados2(){
+        console.log(`----------Cadastro de Usuarios-----------------------------`);
+        console.log(`Nome: ${this.nome}`);
+        console.log(`Data de Nascimento: ${this.idade.toLocaleDateString('pt-BR')}`);
+        console.log(`E-mail: ${this.email}`);
+        console.log(`Endereço: ${this.endereco}`);
+        console.log(`Livros Solicitados: ${}`);
+        console.log(`----------------------------------------------------------`);
+    }
 }
-const user1 = new Usuarios('Alex', 28, 'a.g.gomesalex@gmail.com', 'rua macae , 130', livro2, livro1, new Date("2023-2-22"))
-console.log(user1)
+const user1 = new Usuarios('Alex', new Date('1995-8-24'), 'a.g.gomesalex@gmail.com', 'rua macae , 130', livro2, livro1, new Date("2023-2-22"))
+//console.log(user1)
 
 class EmprestimosLivros {
     info_usuario: Usuarios;
@@ -75,4 +101,4 @@ class EmprestimosLivros {
     }
 }
 const relatorio_emprestimos = new EmprestimosLivros(user1, livro1, new Date("2023-2-22"), new Date("2023-11-20"))
-console.log(relatorio_emprestimos)
+//console.log(relatorio_emprestimos)
